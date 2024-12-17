9f85e5ae3e0883b332934947b99d95efea542dc3
AnasGabbadi <anas0gabbadi@gmail.com>
2024-12-17 16:43:12 +0100

    Ajout des fonctionnalités modifier et supprimer pour les clients

--- MiniProjetGestionHotel/class.h
+++ MiniProjetGestionHotel/class.h
@@ -9,7 +9,7 @@ using namespace std;
 class Client {
 public:
     void ajouterClient(string CIN, string nom, string prenom, string tel) {
-        ofstream file("clients.txt", ios::app); // Ouverture en mode ajout
+        ofstream file("clients.txt", ios::app);
         if (file.is_open()) {
             int client_id = genererId("clients.txt");
             file << "ID : " << client_id << " , " << "CIN : " << CIN << " , " << "Nom : " << nom << " , " << "Prenom : " << prenom << " , " << "Tel : " << tel << endl;
@@ -29,6 +29,75 @@ public:
                 cout << line << endl;
             }
             file.close();
+        }
+        else {
+            cerr << "Erreur : Impossible d'ouvrir le fichier clients.txt" << endl;
+        }
+    }
+
+    void modifierClient(string CIN, string newNom, string newPrenom, string newTel) {
+        vector<string> lignes;
+        ifstream file_in("clients.txt");
+        bool found = false;
+
+        if (file_in.is_open()) {
+            string line;
+            while (getline(file_in, line)) {
+                if (line.find(CIN) != string::npos) { 
+                    ostringstream modifiedLine;
+                    modifiedLine << CIN << ",Nom:" << newNom << ",Prenom:" << newPrenom << ",Tel:" << newTel;
+                    lignes.push_back(modifiedLine.str());
+                    found = true;
+                }
+                else {
+                    lignes.push_back(line); 
+                }
+            }
+            file_in.close();
+
+            ofstream file_out("clients.txt", ios::trunc);
+            for (const string& l : lignes) {
+                file_out << l << endl;
+            }
+            file_out.close();
+
+            if (found)
+                cout << "Client modifie avec succes !" << endl;
+            else
+                cout << "Client avec CIN " << CIN << " non trouve." << endl;
+        }
+        else {
+            cerr << "Erreur : Impossible d'ouvrir le fichier clients.txt" << endl;
+        }
+    }
+
+    void supprimerClient(string CIN) {
+        vector<string> lignes;
+        ifstream file_in("clients.txt");
+        bool found = false;
+
+        if (file_in.is_open()) {
+            string line;
+            while (getline(file_in, line)) {
+                if (line.find(CIN) == string::npos) {
+                    lignes.push_back(line); 
+                }
+                else {
+                    found = true;
+                }
+            }
+            file_in.close();
+
+            ofstream file_out("clients.txt", ios::trunc);
+            for (const string& l : lignes) {
+                file_out << l << endl;
+            }
+            file_out.close();
+
+            if (found)
+                cout << "Client supprime avec succes !" << endl;
+            else
+                cout << "Client avec CIN " << CIN << " non trouve." << endl;
         }
         else {
             cerr << "Erreur : Impossible d'ouvrir le fichier clients.txt" << endl;
--- MiniProjetGestionHotel/main.cpp
+++ MiniProjetGestionHotel/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "class.h"
+#include "class.h"  
 
 using namespace std;
 
@@ -8,16 +8,21 @@ int main() {
 
     int choix;
     do {
+        cout << "-----------------";
         cout << "\nMenu :\n";
+        cout << "-----------------" << endl;
         cout << "1. Ajouter un client\n";
         cout << "2. Afficher les clients\n";
+        cout << "3. Modifier un client\n";
+        cout << "4. Supprimer un client\n";
         cout << "0. Quitter\n";
+        cout << "-----------------" << endl;
         cout << "Choix : ";
         cin >> choix;
-
+        cout << "-----------------" << endl;
         switch (choix) {
         case 1: {
-            string CIN ,nom, prenom, tel;
+            string CIN, nom, prenom, tel;
             cout << "CIN : ";
             cin >> CIN;
             cout << "Nom : ";
@@ -32,6 +37,30 @@ int main() {
         case 2:
             client.afficherClients();
             break;
+        case 3: {
+            string CIN, newNom, newPrenom, newTel;
+            client.afficherClients();
+            cout << "-----------------" << endl;
+            cout << "Entrez le CIN du client a modifier : ";
+            cin >> CIN;
+            cout << "Nouveau Nom : ";
+            cin >> newNom;
+            cout << "Nouveau Prenom : ";
+            cin >> newPrenom;
+            cout << "Nouveau Tel : ";
+            cin >> newTel;
+            client.modifierClient(CIN, newNom, newPrenom, newTel);
+            break;
+        }
+        case 4: {
+            client.afficherClients();
+            cout << "-----------------" << endl;
+            string CIN;
+            cout << "Entrez le CIN du client a supprimer : ";
+            cin >> CIN;
+            client.supprimerClient(CIN);
+            break;
+        }
         case 0:
             cout << "Au revoir !" << endl;
             break;
